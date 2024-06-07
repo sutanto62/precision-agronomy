f388eefc945a70d4722a56987fb096d7c1e5cb5a
Cayadi <c@sutanto.or.id>
2024-06-07 11:34:36 +0700

    add 1
    add SwitchView
    
    add SwitchView Cayadi

---
+++ gitattributes/SectionView.swift
@@ -0,0 +1,16 @@
+//
+//  SectionView.swift
+//  gitattributes
+//
+//  Created by Cayadi Sutanto on 07/06/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct SectionView: View {
+    // do something here
+    var body: some View {
+            Text("Hello, World! Cayadi")
+        }
+}
