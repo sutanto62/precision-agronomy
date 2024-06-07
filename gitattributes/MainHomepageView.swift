883196bfc7894b143168ed573508cba1e085d6e7
Cayadi <c@sutanto.or.id>
2024-06-07 11:55:08 +0700

    add homepage

---
+++ gitattributes/MainHomepageView.swift
@@ -0,0 +1,16 @@
+//
+//  SectionView.swift
+//  gitattributes
+//
+//  Created by Cayadi Sutanto on 07/06/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct MainHomepageView: View {
+    // do something here
+    var body: some View {
+            Text("It is homepage")
+        }
+}
